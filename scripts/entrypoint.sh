f6ad81d4ab98fc5834122a0b8e90bf18fd8c0954
Алексей Неустроев <hbg@yandex.ru>
2025-11-28 15:36:40 +0300

    fix entrypoint.sh v6

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,32 @@ set -e
 
 echo "Edit frontend vite.config.ts config file..."
 sed -i '/preview: {/a\    allowedHosts: true,' node_modules/@hexlet/project-devops-deploy-crud-frontend/vite.config.ts &
+cat > node_modules/@hexlet/project-devops-deploy-crud-frontend/vite.config.ts << 'EOF'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'dobro10k2.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
+EOF
+
+cat node_modules/@hexlet/project-devops-deploy-crud-frontend/vite.config.ts
 
 echo "Starting services..."
 
@@ -19,7 +45,7 @@ echo "FastAPI is UP"
 
 # Запускаем фронтенд в фоне  
 echo "Starting frontend..."
-VITE_API_URL=/api npx vite --host 0.0.0.0 --port 5173 &
+VITE_API_URL=/api npx start-hexlet-devops-deploy-crud-frontend &
 
 # Wait for frontend
 until curl -s http://127.0.0.1:5173 >/dev/null; do
