b02df2a4f2b5bf255791c165d1f5ab3c84291a43
Алексей Неустроев <hbg@yandex.ru>
2025-11-28 16:29:30 +0300

    fix entrypoint.sh v10

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,35 @@ set -e
 echo "Patching built frontend bundle URLs..."
 sed -i 's|http://localhost:8080/api|https://dobro10k2.onrender.com/api|g' \
   /app/node_modules/@hexlet/project-devops-deploy-crud-frontend/dist/assets/index-*.js
+cat > /app/node_modules/@hexlet/project-devops-deploy-crud-frontend/vite.config.js << 'EOF'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+const API_URL = process.env.API_URL || 'https://dobro10k2.onrender.com';
+
+export default defineConfig({
+  plugins: [react()],
+
+  preview: {
+    allowedHosts: true,
+    port: 5173,
+    host: "0.0.0.0",
+  },
+
+  server: {
+    allowedHosts: ["dobro10k2.onrender.com"],
+    port: 5173,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+EOF
 
 echo "Starting services..."
 
