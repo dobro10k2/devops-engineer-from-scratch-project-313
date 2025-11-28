43666a607ab63cb7c4e1446a7b90f0fbec59de9c
Алексей Неустроев <hbg@yandex.ru>
2025-11-28 16:18:55 +0300

    fix entrypoint.sh v9

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -20,7 +20,7 @@ echo "FastAPI is UP"
 
 # Запускаем фронтенд в фоне  
 echo "Starting frontend..."
-VITE_API_URL=/api npx start-hexlet-devops-deploy-crud-frontend &
+npx start-hexlet-devops-deploy-crud-frontend &
 
 # Wait for frontend
 until curl -s http://127.0.0.1:5173 >/dev/null; do
