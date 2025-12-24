cfba0cd75ff27dfedb4ba5075e31ae838a9185d9
Алексей Неустроев <hbg@yandex.ru>
2025-12-24 23:33:28 +0300

    edit app/db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import os
 
 from sqlmodel import Session, SQLModel, create_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres:", "postgresql:")
 
 # PostgreSQL engine using psycopg2-binary
 engine = create_engine(DATABASE_URL, echo=True)
@@ -16,4 +16,3 @@ def get_session():
     """Provide a session for CRUD operations"""
     with Session(engine) as session:
         yield session
-
