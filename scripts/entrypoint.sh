b72d0f9d09c72080064b933a918fe19692cad835
Алексей Неустроев <hbg@yandex.ru>
2025-12-28 00:13:40 +0300

    final commit

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,40 @@
 #!/bin/sh
 set -e
 
+#echo "=== Patching frontend bundle ==="
+#
+#find /app/node_modules/@hexlet/project-devops-deploy-crud-frontend/dist \
+#  -type f -name "*.js" \
+#  -exec sed -i 's|http://localhost:8080/api|https://dobro10k2.onrender.com/api|g' {} +
+
+cat > /app/node_modules/@hexlet/project-devops-deploy-crud-frontend/vite.config.js << 'EOF'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+const API_URL = process.env.API_URL || 'https://dobro10k2.onrender.com';
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: true,
+    port: 5173,
+    host: "0.0.0.0",
+  },
+  server: {
+    allowedHosts: ["dobro10k2.onrender.com"],
+    port: 5173,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+EOF
+
 echo "=== Starting backend ==="
 uv run fastapi dev --host 0.0.0.0 --port 8080 &
 
